e62933b1352207252a7fdb4df33cd6692a39f6ff
trungdohoang <hoangtrung15012k@gmail.com>
2021-12-14 15:21:32 +0700

    done footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import './footer.scss'
+import { logo, footerBg } from '../../assets'
+import { Link } from 'react-router-dom'
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer
+            className="footer"
+            style={{ backgroundImage: `url(${footerBg})` }}
+        >
+            <div className="footer__content container">
+                <div className="footer__content__logo">
+                    <div className="logo">
+                        <img src={logo} alt="Logo" />
+                        <Link to="/" >tMovie</Link>
+                    </div>
+                </div>
+                <div className="footer__content__menus">
+                    <div className="footer__content__menu">
+                        <Link to="/" >Home</Link>
+                        <Link to="/" >Contact us</Link>
+                        <Link to="/" >Term of Services</Link>
+                        <Link to="/" >About us</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/" >Live</Link>
+                        <Link to="/" >FAQ</Link>
+                        <Link to="/" >Premium</Link>
+                        <Link to="/" >Privacy policy</Link>
+                    </div>
+                    <div className="footer__content__menu">
+                        <Link to="/" >You must watch</Link>
+                        <Link to="/" >Recent releases</Link>
+                        <Link to="/" >Top IMDB</Link>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
